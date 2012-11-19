686e2d2ca172c82c4c2f79c978129e50b8883249
Andreas Hammar <andreas.hammar@jayway.com>
2012-11-19 23:45:23 +0100

    added wallet demo

---
+++ Wp8GeoDemo/Wp8GeoDemo/DealPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Wp8GeoDemo
+{
+    public partial class DealPage : PhoneApplicationPage
+    {
+        public DealPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            string dealName;
+            NavigationContext.QueryString.TryGetValue("DealName", out dealName);
+            if (string.IsNullOrEmpty(dealName)) return;
+
+            DealName.Text = dealName;
+        }
+    }
+}
--- Wp8GeoDemo/Wp8GeoDemo/MainPage.xaml.cs
+++ Wp8GeoDemo/Wp8GeoDemo/MainPage.xaml.cs
@@ -2,10 +2,14 @@
 using System.Device.Location;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Maps.Services;
 using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+using Microsoft.Phone.Wallet;
 using Windows.Devices.Geolocation;
 
 namespace Wp8GeoDemo
@@ -38,8 +42,8 @@ namespace Wp8GeoDemo
             };
 
             geolocator.StatusChanged += geolocator_StatusChanged;
-            geolocator.PositionChanged += HandlePositionChangedSimple;
-            //geolocator.PositionChanged += HandlePositionChangedWithReverseGeo;
+            //geolocator.PositionChanged += HandlePositionChangedSimple;
+            geolocator.PositionChanged += HandlePositionChangedWithReverseGeo;
         }
 
         private void HandlePositionChangedSimple(Geolocator sender, PositionChangedEventArgs args)
@@ -63,6 +67,23 @@ namespace Wp8GeoDemo
             {
                 Dispatcher.BeginInvoke(() => DisplayTimedStatus(text));
             }
+        }
+
+        private static void ShowMovementToast(string text)
+        {
+            var toast = new ShellToast
+                            {
+                                Title = "movement!",
+                                Content = text
+                            };
+            toast.Show();
+        }
+
+        private void DisplayTimedStatus(string text)
+        {
+            string textWithDate = string.Format("{0:HH:mm.ss.f}{1}{2}",
+                                                DateTime.Now, Environment.NewLine, text);
+            StatusText.Text = textWithDate;
         }
 
         private void HandlePositionChangedWithReverseGeo(Geolocator sender, PositionChangedEventArgs args)
@@ -106,32 +127,16 @@ namespace Wp8GeoDemo
             }
         }
 
-        private static void ShowMovementToast(string text)
-        {
-            var toast = new ShellToast
-                            {
-                                Title = "movement!",
-                                Content = text
-                            };
-            toast.Show();
-        }
-
         void geolocator_StatusChanged(Geolocator sender, StatusChangedEventArgs args)
         {
             Debug.WriteLine("geo status: {0}", args.Status);
-        }
-
-        private void DisplayTimedStatus(string text)
-        {
-            string textWithDate = string.Format("{0:HH:mm.ss.f}{1}{2}",
-                DateTime.Now, Environment.NewLine, text);
-            StatusText.Text = textWithDate;
         }
 
         private static void DebugWrite(string text)
         {
             Debug.WriteLine("{0:HH:mm.ss.fff}: {1}", DateTime.Now, text);
         }
+
 
         // Sample code for building a localized ApplicationBar
 
@@ -150,5 +155,26 @@ namespace Wp8GeoDemo
         //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
         //    ApplicationBar.MenuItems.Add(appBarMenuItem);
         //}
+
+        private async void AddWalletItem_OnTap(object sender, GestureEventArgs e)
+        {
+            var dealId = "dealId";
+            var walletItem = Wallet.FindItem(dealId);
+            if (walletItem != null)
+            {
+                Wallet.Remove(walletItem);
+            }
+
+            var deal = new Deal(dealId)
+                           {
+                               Code = "872394875",
+                               Description = "Really cheap turn-by-turn directions to our office.",
+                               ExpirationDate = DateTime.Now.AddDays(7),
+                               MerchantName = "Jayway",
+                               DisplayName = "Directions"
+                           };
+            deal.NavigationUri = new Uri("/DealPage.xaml?DealName=" + deal.DisplayName, UriKind.Relative);
+            await deal.SaveAsync();
+        }
     }
 }
